2c175df9ff860f5a9a9e32077456d5a6ca3827b5
BASTIAN IGNACIO BASSO AGUIRRE <bbasso@utem.cl>
2025-07-05 18:54:44 -0400

    Funciones en tiempo real para agregar limitantes en campos de llenado de completar info de la targeta + cambios minimos a verificadores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@
 import { redirect } from 'next/navigation';
 
 export default function CheckoutRedirect() {
-  redirect(`/checkout`);
+  redirect(`/checkout/1`);
 }
 // Se redirige a checkout
--- src/components/utilities/Validators.ts
+++ src/components/utilities/Validators.ts
@@ -6,12 +6,14 @@ export const mpvalidators: Record<string, (value: string) => boolean> = {
     // DD/MM
     "form-checkout__expirationDate": (v) => /^(0[1-9]|1[0-2])\/\d{2}$/.test(v),
     // XYZ / WXYZ
-    "form-checkout__securityCode": (v) => /^[0-9]{3,4}$/.test(v),
+    "form-checkout__securityCode": (v) => /^\d{3,4}$/.test(v),
     // No vacio
-    "form-checkout__cardholderName": (v) => /^[A-Za-z]+(?: [A-Za-z]+)*$/.test(v),
+    "form-checkout__cardholderName": (v) => /^[A-Za-zÁÉÍÓÚáéíóúÑñ]+(?: [A-Za-zÁÉÍÓÚáéíóúÑñ]+)*$/.test(v.trim()),
     // Formato correo@example.com
     "form-checkout__cardholderEmail": (v) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/.test(v),
     // No vacio
-    "form-checkout__identificationNumber": (v) => /^\d{7,8}-[\dkK]$/.test(v) ||
-        v.trim().length === 9,
+    "form-checkout__identificationNumber": (v) => {
+        const clean = v.replace(/\./g, "");
+        return /^\d{7,8}-[\dkK]$/.test(clean) || clean.trim().length === 9;
+    },
 };
