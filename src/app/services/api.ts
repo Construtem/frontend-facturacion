398b3729c5dafd6ab7667fee030bfc56717f8a1f
Jinja <hromeror@utem.cl>
2025-07-15 14:20:39 -0400

    Cambios en api y payment_data

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,14 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 interface RetryConfig extends AxiosRequestConfig {
   __isRetryRequest?: boolean;
 }
 
 const customConfig: RetryConfig = {
   __isRetryRequest: false,
-  baseURL,
   headers: {
     'Content-Type': 'application/json'
   },
