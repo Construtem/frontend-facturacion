ec68c2fce1434065835eac90f85556215049d16d
Andres <andres.gomez.rodriguez2003@gmail.com>
2025-07-19 03:31:33 -0400

    Usando archivo not-found basico, en local está buildeando por algun motivo xddd

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+// app/not-found.tsx
+'use client';
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>404 - Página no encontrada</h1>
+      <p>Lo sentimos, no pudimos encontrar lo que buscas.</p>
+    </div>
+  );
+}
