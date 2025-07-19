9eb2d2d33f794a8b577ecddc5421d46604442e10
Jinja <hromeror@utem.cl>
2025-07-19 15:12:40 -0400

    Quitando comentarios debug en services

--- src/app/services/Summary-call.ts
+++ src/app/services/Summary-call.ts
@@ -1,8 +1,6 @@
- import api from './api';
+import api from './api';
 
 export const getPaymentData = async (id: number) => {
-    console.log('Hola mundo');
-    // Debugging log
     const response = await api.get(`/api/Sumary-data/${id}`);
     console.log('Response from backend:', response.data);
     return response;
